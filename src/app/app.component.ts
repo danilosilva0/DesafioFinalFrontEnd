8e84b365b08c497bb21181427200c4eaeae4ef52
Danilo Silva <dadsv8@gmail.com>
2024-07-18 22:54:13 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+// import { NotificationService } from './services/notification.service';
+import { InputValidationComponent } from './components/input-validation/input-validation.component'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, InputValidationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'desafio-final-front';
+
+  // constructor(private notificationService: NotificationService) {}
+
+  // // Método exemplo para atualizar notificações (se necessário)
+  // updateNotificationCount() {
+  //   // Você pode chamar um método no serviço para atualizar o número de notificações
+  //   this.notificationService.getNotificationCount().subscribe(count => {
+  //     console.log('Número de notificações:', count);
+  //   });
+  // }
 }
+
